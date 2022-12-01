94bb960f6f93a2718d1445988b76d36399efb1e4
ayau-akk <118360890+ayau-akk@users.noreply.github.com>
2022-12-01 11:40:16 +0600

    Add files via upload

---
+++ hw2.1.php
@@ -0,0 +1,9 @@
+<?php
+
+
+date_default_timezone_set('Europe/London');
+echo 'London:' . date('H:i:s d.m.Y').'<br>';
+
+date_default_timezone_set('Europe/London');
+echo 'Paris:' . date('H:i:s d.m.Y').'<br>';
+?>
